d2d6736d0c994edc1f0234f522c1714d6b241725
centurions90 <chrisaaron.cheng@gmail.com>
2022-12-05 20:46:10 +0800

    reworked entire code to be simpler. physics not implemented yet

--- game/src/cloth2D.c
+++ game/src/cloth2D.c
@@ -1,36 +1,28 @@
 #include <stdlib.h>
-#include <stdbool.h>
 #include "raylib.h"
 #include "raymath.h"
 
 //----------------------------------------------------------------------------------
-// Shared Variables Definition (global)
-//----------------------------------------------------------------------------------
-typedef struct cloth {
-    int N;
-    Vector2** positions;
-    Vector2** lastPositions;
-    int* pinned;
-    int pinnedCount;
-} Cloth;
-
-//----------------------------------------------------------------------------------
-// Local Variables Definition (local to this module)
+// Variables Definition
 //----------------------------------------------------------------------------------
 static const int screenWidth = 1920;
 static const int screenHeight = 1080;
-static Cloth cloth;
-static Vector2 spacing;
-static float drag;
+static int N;                           // Cloth resolution
+static float size = 800;
+static float* x;
+static float* y;
+static int* pinned;
+static float spacing;
+static int iterations = 1;
 
 //----------------------------------------------------------------------------------
-// Local Functions Declaration
+// Functions Declaration
 //----------------------------------------------------------------------------------
 
 static void UpdateDrawFrame(void);  // Update and draw one frame
 static void CreateCloth(int N);     // Create cloth with an NxN resolution
-static void UpdateCloth();
-static bool isPinned(int* list, int count, int index);
+static void FreeCloth(int N);       // Frees cloth from memory
+static void UpdateCloth();          // Update cloth physics
 
 //----------------------------------------------------------------------------------
 // Main entry point
@@ -42,18 +34,10 @@ int main(int argc, char* argv[])
     InitWindow(screenWidth, screenHeight, "2D Cloth Simulation");
     SetTargetFPS(60);
 
-    int N;
-
     if (argc >= 2) {
         N = atof(argv[1]);
     } else {
         N = 10;
-    }
-
-    if (argc >= 3) {
-        drag = atof(argv[2]);
-    } else {
-        drag = 0.004 * N;
     }
 
     CreateCloth(N);
@@ -70,14 +54,7 @@ int main(int argc, char* argv[])
 
     // De-Initialization
     //--------------------------------------------------------------------------------------
-    free(cloth.pinned);
-    for (int i = 0; i < N; i++) {
-        free(cloth.positions[i]);
-        free(cloth.lastPositions[i]);
-    }
-
-    free(cloth.positions);
-    free(cloth.lastPositions);
+    FreeCloth(N);
 
     // Unload global data loaded
 
@@ -99,20 +76,23 @@ static void UpdateDrawFrame(void)
 
     ClearBackground(BLACK);
 
-    for (int i = 0; i < cloth.N; i++) {
-        for (int j = 0; j < cloth.N; j++) {
-            Vector2 point = cloth.positions[i][j];
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            int index = i * N + j;
+            DrawCircle(x[index], y[index], 3, RED);
 
-            DrawCircle(point.x, point.y, 3, RED);
-            
-            if (i + 1 < cloth.N) {
-                Vector2 next = cloth.positions[i + 1][j];
-                DrawLine(point.x, point.y, next.x, next.y, RED);
+            if (j + 1 < N)
+            {
+                int next = index + 1;
+                DrawLine(x[index], y[index], x[next], y[next], RED);
             }
 
-            if (j + 1 < cloth.N) {
-                Vector2 next = cloth.positions[i][j + 1];
-                DrawLine(point.x, point.y, next.x, next.y, RED);
+            if (i + 1 < N)
+            {
+                int next = index + N;
+                DrawLine(x[index], y[index], x[next], y[next], RED);
             }
         }
     }
@@ -123,119 +103,44 @@ static void UpdateDrawFrame(void)
     //----------------------------------------------------------------------------------
 }
 
-static void CreateCloth(int N) {
-    Vector2 center = {screenWidth / 2, screenHeight / 2 - 100};
-    Vector2 offset = {400, 400};
-    spacing.x = ((1.0 / (N - 1) - 0.5) * 2) * offset.x - ((0 - 0.5) * 2) * offset.x;
-    spacing.y = ((1.0 / (N - 1) - 0.5) * 2) * offset.y - ((0 - 0.5) * 2) * offset.y;
+static void CreateCloth(int N)
+{
+    float originX = screenWidth / 2.0 - size / 2.0;
+    float originY = 150;
+    spacing = 1.0 / N * size;
 
-    cloth.N = N;
-    cloth.pinned = (int*)malloc(N * sizeof(int));
-    for (int i = 0; i < N; i++) {
-        cloth.pinned[i] = i;
-    }
-    cloth.pinnedCount = N;
-    cloth.positions = (Vector2**)malloc(N * sizeof(Vector2*));
-    cloth.lastPositions = (Vector2**)malloc(N * sizeof(Vector2*));
+    x = malloc(N * N * sizeof(float));
+    y = malloc(N * N * sizeof(float));
+    pinned = malloc(N * N * sizeof(int));
 
-    for (int i = 0; i < N; i++) {
-        cloth.positions[i] = (Vector2*)malloc(N * sizeof(Vector2));
-        cloth.lastPositions[i] = (Vector2*)malloc(N * sizeof(Vector2));
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (i == 0)
+            {
+                pinned[j] = 1;
+            }
+            else
+            {
+                pinned[i * N + j] = 0;
+            }
 
-        for (int j = 0; j < N; j++) {
-            cloth.positions[i][j] = (Vector2){
-                .x = center.x + (((float)j / (N - 1) - 0.5) * 2) * offset.x,
-                .y = center.y + (((float)i / (N - 1) - 0.5) * 2) * offset.y
-            };
-            cloth.lastPositions[i][j] = cloth.positions[i][j];
+            int index = i * N + j;
+            x[index] = originX + spacing * j;
+            y[index] = originY + spacing * i;
         }
     }
 }
 
-static void UpdateCloth() {
-    for (int i = 0; i < cloth.N; i++) {
-        for (int j = 0; j < cloth.N; j++) {
-            if (!isPinned(cloth.pinned, cloth.pinnedCount, i * cloth.N + j)) {
-                Vector2 mousePos = GetMousePosition();
-                Vector2 mouseDelta = GetMouseDelta();
-                mouseDelta = Vector2Scale(mouseDelta, 2);
-                mouseDelta = Vector2ClampValue(mouseDelta, -spacing.x, spacing.x);
-
-                float distance = Vector2Distance(mousePos, cloth.positions[i][j]);
-
-                if (distance < 15) {
-                    cloth.lastPositions[i][j] = Vector2Subtract(cloth.positions[i][j], mouseDelta);
-                }
-            }
-        }
-    }
-
-    for (int i = 0; i < cloth.N; i++) {
-        for (int j = 0; j < cloth.N; j++) {
-            if (!isPinned(cloth.pinned, cloth.pinnedCount, i * cloth.N + j)) {
-                Vector2 newPos;
-
-                // Velocity
-                newPos = Vector2Add(cloth.positions[i][j], Vector2Scale(Vector2Subtract(cloth.positions[i][j], cloth.lastPositions[i][j]), 1.0 - drag));
-
-                // Gravity
-                newPos = Vector2Add(newPos, (Vector2) { .x = 0, .y = 981 * (1.0 - drag) * 0.02 * 0.02 });
-
-                cloth.lastPositions[i][j] = cloth.positions[i][j];
-                cloth.positions[i][j] = newPos;
-            }
-        }
-    }
-
-    for (int i = 0; i < cloth.N; i++) {
-        for (int j = 0; j < cloth.N; j++) {
-            // Cloth
-            int x = 1;
-            int y = 0;
-                
-            while (x >= 0 && y <= 1) {
-                if (
-                    !isPinned(cloth.pinned, cloth.pinnedCount, (i + y) * cloth.N + (j + x)) &&
-                    j + x < cloth.N &&
-                    i + y < cloth.N
-                ) {
-                    Vector2 diff = Vector2Subtract(cloth.positions[i][j], cloth.positions[i + y][j + x]);
-                    float dist = Vector2Length(diff);
-                    float diffFactor;
-
-                    if (y == 0) {
-                        diffFactor = (spacing.x - dist) / dist;
-                    } else {
-                        diffFactor = (spacing.y - dist) / dist;
-                    }
-
-                    Vector2 offset = Vector2Scale(diff, diffFactor * 0.5);
-
-                    cloth.positions[i + y][j + x] = Vector2Subtract(cloth.positions[i + y][j + x], offset);
-
-                    if (!isPinned(cloth.pinned, cloth.pinnedCount, i * cloth.N + j)) {
-                        cloth.positions[i][j] = Vector2Add(cloth.positions[i][j], offset);
-                    } else {
-                        cloth.positions[i + y][j + x] = Vector2Subtract(cloth.positions[i + y][j + x], offset);
-                    }
-                }
-
-                x--;
-                y++;
-            }
-        }
-    }
+static void FreeCloth(int N)
+{
+    free(x);
+    free(y);
+    free(pinned);
 }
 
-static bool isPinned(int* list, int count, int index) {
-    bool result = false;
-
-    for (int i = 0; i < count; i++) {
-        if (list[i] == index) {
-            result = true;
-            break;
-        }
-    }
-
-    return result;
+static void UpdateCloth()
+{
+    
 }
