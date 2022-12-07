01d70303a8ce448ed594e6b92fd27b743c3d9dfb
centurions90 <chrisaaron.cheng@gmail.com>
2022-12-07 17:54:03 +0800

    removed useless code

--- game/src/cloth2D.c
+++ game/src/cloth2D.c
@@ -2,12 +2,6 @@
 #include "raylib.h"
 #include "raymath.h"
 
-#if defined(PLATFORM_DESKTOP)
-#define GLSL_VERSION            330
-#else   // PLATFORM_RPI, PLATFORM_ANDROID, PLATFORM_WEB
-#define GLSL_VERSION            100
-#endif
-
 //----------------------------------------------------------------------------------
 // Variables Definition
 //----------------------------------------------------------------------------------
